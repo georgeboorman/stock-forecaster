e177f4906e43cd626d55cfc76cdb58cdf7e7f1df
georgeboorman <georgeboorman@gmail.com>
2025-08-14 11:12:29 +0100

    Fix error in extract.py

--- extract.py
+++ extract.py
@@ -97,7 +97,7 @@ def save_to_csv(data_dict, filename="stocks.csv"):
     """
     combined_df = pd.concat(data_dict.values(), ignore_index=True)
     write_header = not os.path.exists(filename)
-    combined_df.to_csv(filename, mode='a', header=write_header, index=False, line_terminator='\n')
+    combined_df.to_csv(filename, mode='a', header=write_header, index=False)
     print(f"Data appended to {filename}")
 
 if __name__ == "__main__":
